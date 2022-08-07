c37ebd7d53aa8565223495378a575388bbbdc07d
Dmitry Rotanin <rotanin@gmail.com>
2022-08-07 18:38:29 +0300

    Pagination, change er msg, del user path

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 class ReadOnly(permissions.BasePermission):
     """Исключительно GET запрос."""
 
-    message = 'Создание и редактирование групп через API - ЗАПРЕЩЕНО!'
+    message = 'Создание и редактирование - ЗАПРЕЩЕНО!'
 
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS
